2a55369831fbe478fc2abdb14e0de9cb5f8a4b52
Brune Miguel <bstfreire@gmail.com>
2020-09-09 16:11:13 -0300

    Acessando com o DOM

--- exercises/5_1/arquivoJs.js
+++ exercises/5_1/arquivoJs.js
@@ -1,9 +1,15 @@
 let paragraph = document.getElementById("paragraph");
 paragraph.style.color = "red";
 
-document.getElementById("page-title").innerText = "Novo Título modificado com getElementById!!!"
+document.getElementById("page-title").innerText = "Novo Título modificado com getElementById!!!";
 
-document.getElementById("second-paragraph").innerText = "Começamos um novo módulo e aprenderemos a manipular elementos utilizando getElement."
+document.getElementById("second-paragraph").innerText = "Começamos um novo módulo e aprenderemos a manipular elementos utilizando getElement.";
 
-document.getElementById("subtitle").innerText = "Veja as maravilhas do GetElement"
+document.getElementById("subtitle").innerText = "Veja as maravilhas do GetElement";
+
+
+let paragrafo = document.getElementsByTagName("p")
+for (let i = 0; i < paragrafo.length; i += 1){
+    paragrafo[i].style.fontFamily = 'Franklin Gothic Medium', 'Arial Narrow', 'Arial', 'sans-serif';
+}
 
